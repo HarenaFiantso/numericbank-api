799262d7f2b54562c7354dcf420cf79643a9e98b
tanjonaaa <hei.tanjona@gmail.com>
2024-03-29 22:44:57 +0300

    chore: create repository method to call function

--- src/main/java/com/digital/bank/repository/AccountRepository.java
+++ src/main/java/com/digital/bank/repository/AccountRepository.java
@@ -1,31 +1,63 @@
 package com.digital.bank.repository;
 
+import com.digital.bank.component.AccountStatementComponent;
 import com.digital.bank.model.Account;
 import com.digital.bank.util.drr.utility.DreamReflectRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class AccountRepository extends DreamReflectRepository<Account> {
-    public AccountRepository(Connection connection) {
-        super(connection);
+  private final Connection connection;
+
+  public AccountRepository(Connection connection, Connection connection1) {
+    super(connection);
+    this.connection = connection1;
+  }
+
+  public List<AccountStatementComponent> generateAccountStatement(
+      String id, LocalDate startDate, LocalDate endDate) throws SQLException {
+    List<AccountStatementComponent> results = new ArrayList<>();
+
+    CallableStatement statement =
+        this.connection.prepareCall("{call generate_account_statement(?, ?, ?)}");
+    statement.setString(1, id);
+    statement.setDate(2, Date.valueOf(startDate));
+    statement.setDate(3, Date.valueOf(endDate));
+    ResultSet resultSet = statement.executeQuery();
+
+    while (resultSet.next()) {
+      results.add(
+          AccountStatementComponent.builder()
+              .transactionDate(resultSet.getDate("transaction_date").toLocalDate())
+              .reference(resultSet.getString("reference"))
+              .reason(resultSet.getString("reason"))
+              .debit(resultSet.getDouble("debit"))
+              .credit(resultSet.getDouble("credit"))
+              .balance(resultSet.getDouble("balance"))
+              .build());
     }
-    @Override
-    protected Account mapResultSet(ResultSet resultSet) {
-        try {
-            return Account.builder()
-                    .idAccount(resultSet.getString("id_account"))
-                    .firstName(resultSet.getString("first_name"))
-                    .lastName(resultSet.getString("last_name"))
-                    .birthDate(resultSet.getDate("birth_date"))
-                    .monthlySalary(resultSet.getDouble("monthly_salary"))
-                    .overDrafted(resultSet.getBoolean("over_drafted"))
-                    .build();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+    return results;
+  }
+
+  @Override
+  protected Account mapResultSet(ResultSet resultSet) {
+    try {
+      return Account.builder()
+          .idAccount(resultSet.getString("id_account"))
+          .firstName(resultSet.getString("first_name"))
+          .lastName(resultSet.getString("last_name"))
+          .birthDate(resultSet.getDate("birth_date"))
+          .monthlySalary(resultSet.getDouble("monthly_salary"))
+          .overDrafted(resultSet.getBoolean("over_drafted"))
+          .build();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
